785aef45b9b6b5edeb07f4493a52cefed30a4936
Manan <mananchakma32@gmail.com>
2021-02-10 01:04:14 +0600

    login page design

--- lib/src/views/auth/login/login_form.dart
+++ lib/src/views/auth/login/login_form.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,61 +10,126 @@ class LoginForm extends StatefulWidget {
 
 class _LoginFormState extends State<LoginForm> {
   final _loginFormKey = GlobalKey<FormState>();
-  final userNameController = TextEditingController();
+  final userPasswordController = TextEditingController();
   final userPhoneController = TextEditingController();
-
 
   @override
   void dispose() {
-    userNameController.dispose();
+    userPasswordController.dispose();
     userPhoneController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Form(
-            key: _loginFormKey,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  controller: userNameController,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Username';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: userPhoneController,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please Enter Phone';
-                    }
-                    return null;
-                  },
-                ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 3,
+            child: Container(color: Colors.white,),
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              color: Colors.teal,
+              child: Form(
+                  key: _loginFormKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      UserPhone(),
+                      UserPassword(),
+                      Container(
+                        alignment: Alignment.center,
+                        child: FractionallySizedBox(
+                          widthFactor: 0.7,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                if (_loginFormKey.currentState.validate()) {
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content: Text('Successful')));
+                                }
+                              },
+                              child: Text('Login')),
+                        ),
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/registration');
+                          },
+                          child: Text('Open an account')),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/dashboard');
+                          },
+                          child: Text('dashboard'))
+                    ],
+                  )),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(color: Colors.white,),
+          ),
 
-                ElevatedButton(
-                    onPressed: () {
-                      if (_loginFormKey.currentState.validate()) {
-                        Scaffold.of(context)
-                            .showSnackBar(SnackBar(content: Text('Successful')));
-                      }
-                    },
-                    child: Text('Login')
-                ),
-                GestureDetector(onTap: (){
-                  Navigator.pushNamed(context, '/registration');
-                }, child: Text('Open an account')),
-                GestureDetector(onTap: (){
-                  Navigator.pushNamed(context, '/dashboard');
-                }, child: Text('dashboard'))
+        ],
 
-              ],
-            )),
+      ),
+    );
+  }
+}
+
+class UserPhone extends StatelessWidget {
+  final userPhoneController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return LoginFormInput(userPhoneController, 'phone', '017********');
+  }
+}
+
+class UserPassword extends StatelessWidget {
+  final userPasswordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return LoginFormInput(userPasswordController, 'password', '');
+  }
+
+}
+
+class LoginButton {}
+
+class LoginFormInput extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String labelHint;
+
+  LoginFormInput(this.controller, this.labelText, this.labelHint);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: FractionallySizedBox(
+        widthFactor: 0.7,
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: labelText,
+            hintText: labelHint,
+            labelStyle: TextStyle(
+              color: Color(0xFF6200EE),
+            ),
+          ),
+          controller: controller,
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Please Enter Username';
+            }
+            return null;
+          },
+        ),
       ),
     );
   }
